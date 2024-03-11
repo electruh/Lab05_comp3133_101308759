920e9554e045064951d39e8fe41a7cb009480ad5
allanisjoiceelectraj.sumaya <allanissumaya22@gmail.com>
2024-03-10 22:47:10 -0400

    Lab 5

---
+++ exercise-1/hello-world.js
@@ -0,0 +1,9 @@
+// var greeter = function (name){
+//     console.log("Hello, " + name + "!");
+// }
+//
+// greeter("Allanis Sumaya");
+var greeter = function (firstName, lastName) {
+    console.log("Hello ".concat(firstName, " ").concat(lastName, "!"));
+};
+greeter("Allanis", "Sumaya");
---
+++ exercise-1/hello-world.ts
@@ -0,0 +1,11 @@
+// var greeter = function (name){
+//     console.log("Hello, " + name + "!");
+// }
+//
+// greeter("Allanis");
+
+let greeter = (firstName,  lastName) => {
+    console.log(`Hello ${firstName} ${lastName}!`);
+}
+
+greeter("Allanis", "Sumaya")
